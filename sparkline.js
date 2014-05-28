4e3cf9dac6d160172e36d8515caeaad84955801b
Ean Bowman <eanbowman@gmail.com>
2014-05-28 10:32:08 -0400

    Initial commit. v0.1
    Created a simple sparkline graph script.

---
+++ sparkline.js
@@ -0,0 +1,36 @@
+// sparkline.js - draws a canvas sparkline
+
+/* Usage: <canvas class="sparkline" onclick="sparkline(this);" data-sparkline="1,2,7,5,2,9,10,3,7,8">Your browser does not support the HTML5 canvas tag.</canvas> */
+/* Where data-sparkline is a list of values representing the data to be displayed. */
+
+function max(array) {
+    return Math.max.apply(Math, array);
+};
+
+function sparkline(obj) {
+    var c = obj;
+    console.log(c);
+    var ctx = c.getContext("2d");
+    var data = c.getAttribute("data-sparkline");
+    var spark = data.split(',');
+    for (a in spark) {
+        spark[a] = parseInt(spark[a], 10);
+    }
+    console.log(max(spark));
+    var ratioW = c.width / spark.length;
+    var ratioH = c.height / max(spark);
+    console.log(ratioW + ', ' + ratioH);
+
+    ctx.beginPath();
+    ctx.lineWidth = "1";
+    ctx.strokeStyle = "green";  // Green path
+    for (i = 0; i < spark.length; i++) {
+        if (i == 0) {
+            // First time
+            ctx.moveTo(0, spark[i] * ratioH);
+        } else {
+            ctx.lineTo(i * ratioW, spark[i] * ratioH);
+        }
+    }
+    ctx.stroke();  // Draw it
+};

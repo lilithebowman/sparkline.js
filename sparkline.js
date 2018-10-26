10404970c2703366027f3b178eb9e8acf5238596
Ean Bowman <Ean.Bowman@corusent.com>
2018-10-26 09:42:19 -0400

    [Feature] Added ability to use negatives. Also added demo.html.

--- sparkline.js
+++ sparkline.js
@@ -7,17 +7,26 @@ function max(array) {
 	return Math.max.apply(Math, array);
 };
 
+function min(array) {
+	return Math.min.apply(Math, array);
+}
+
 function sparkline(obj) {
-	var c = obj;
+	var c = obj = _sparklines_[0];
 	var ctx = c.getContext("2d");
 	var data = c.getAttribute("data-sparkline");
 	var spark = data.split(',');
+	var min = Math.min.apply(Math, spark);
+	var max = Math.max.apply(Math, spark);
 	for (a in spark) {
 		spark[a] = parseInt(spark[a], 10);
+		spark[a] += Math.abs(0-min);
 	}
-	var ratioW = ( ( c.width - margin*2 ) * 1 ) / spark.length;
-	var ratioH = ( ( c.height - margin*2 ) * .8 ) / Math.max.apply(Math, spark);
+	var scale = max - min;
 	var margin = 10;
+	var ratioW = ( ( c.width - margin*2 ) * 1 ) / spark.length;
+	var ratioH = ( ( c.height - margin*2 ) * .8 ) / scale;
+	console.log(ratioH);
 	
 	var x = 0;
 	var y = 0;
@@ -30,7 +39,8 @@ function sparkline(obj) {
 	
 	ctx.beginPath();
 	ctx.lineWidth = "6";
-	ctx.arc(margin, c.height - ( spark[0] * ratioH + margin ), 8, 0, 2 * Math.PI);
+	var currentHeight = c.height - ( spark[0] * ratioH + margin );
+	ctx.arc(margin, currentHeight, 8, 0, 2 * Math.PI);
 	ctx.fill();
 	ctx.stroke();
 	for (index in spark) {
@@ -38,10 +48,12 @@ function sparkline(obj) {
 			// First time
 			ctx.beginPath();
 			ctx.lineWidth = "6";
-			ctx.moveTo(margin, c.height - ( spark[index] * ratioH + margin ) );
+			currentHeight = c.height - ( spark[index] * ratioH + margin );
+			ctx.moveTo(margin, currentHeight);
 		} else {
 			x = index * ratioW + margin;
 			y = c.height - ( spark[index] * ratioH + margin );
+			console.log(y);
 			ctx.lineTo(x,y);
 		}
 	}
